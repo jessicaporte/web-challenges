8bd084938649973e49cce39061cdaa11f91d4be2
jessica <jessy_marian91@hotmail.com>
2025-03-13 14:05:54 +0100

    ok

---
+++ JS-createElement/js-createelement_social-media-post/js/index.js
@@ -0,0 +1,40 @@
+console.clear();
+
+function handleLikeButtonClick(event) {
+  const buttonElement = event.target;
+  buttonElement.classList.toggle("post__button--liked");
+}
+
+const likeButton = document.querySelector('[data-js="like-button"]');
+likeButton.addEventListener("click", handleLikeButtonClick);
+
+// Exercise:
+// Use document.createElement() and append another social media post to the body.
+//* <body>
+
+const container = document.body;
+
+const articleElement = document.createElement("article");
+const pElement = document.createElement("p");
+const footerElement = document.createElement("footer");
+const spanElement = document.createElement("span");
+const buttonElement = document.createElement("button");
+
+articleElement.className = "post";
+pElement.className = "post__content";
+footerElement.className = "post__footer";
+spanElement.className = "post__username";
+buttonElement.className = "post__button";
+
+pElement.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+spanElement.textContent = "@username";
+buttonElement.textContent = "▼Like";
+
+articleElement.append(pElement, footerElement);
+footerElement.append(spanElement, buttonElement);
+container.append(articleElement);
+
+buttonElement.addEventListener("click", () => {
+  buttonElement.classList.toggle("post__button--liked");
+});
