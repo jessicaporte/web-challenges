3567de8d42979f2247112dc399928b1509d1f0c0
jessica <jessy_marian91@hotmail.com>
2025-03-17 21:47:43 +0100

    ok

---
+++ JS-Loops/js-loops_forin-select-nav/js/index.js
@@ -0,0 +1,50 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+// Part 1: Creating a Select Input
+
+const languages = {
+  DE: "German",
+  EN: "English",
+  ES: "Spanish",
+  FR: "French",
+  IT: "Italian",
+  NL: "Dutch",
+  PT: "Portuguese",
+  RU: "Russian",
+  ZH: "Chinese",
+};
+
+const select = document.createElement("select");
+select.name = "languages";
+main.append(select);
+
+// --v-- write or modify code below this line --v--
+for (const key in languages) {
+  const option = document.createElement("option");
+  option.value = key;
+
+  option.textContent = languages[key];
+
+  select.append(option);
+
+  // --^-- write or modify code above this line --^--
+}
+
+// Part 2: Creating a Navigation Bar
+const navElement = document.createElement("nav");
+const ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul);
+
+for (const key in nav) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+
+  a.href = nav[key].href;
+  a.textContent = nav[key].text;
+
+  li.append(a);
+  ul.append(li);
+}
