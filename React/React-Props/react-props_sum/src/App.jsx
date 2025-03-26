ba82ab1ff2db9edb762ca72553ff90210d1a799f
jessica <jessy_marian91@hotmail.com>
2025-03-26 11:15:00 +0100

    ejercicio 2

---
+++ React/React-Props/react-props_sum/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return <Sum valueA={1} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {" "}
+      {valueA} + {valueB} = {valueA + valueB}{" "}
+    </h1>
+  );
+}
