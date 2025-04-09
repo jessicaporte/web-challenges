fb8f615a737ca5e96c9f4ecd0ddb04b17599a3d9
jessica <jessy_marian91@hotmail.com>
2025-04-09 13:02:02 +0200

    ejercicio 3

---
+++ next.js/React-Styled-Components/react-styled-components_box/pages/index.js
@@ -0,0 +1,10 @@
+import { StyledBox } from "@/components/BoxWithStyledComponents";
+
+export default function HomePage() {
+  return (
+    <div>
+      <StyledBox />
+      <StyledBox isBlack />
+    </div>
+  );
+}
